63adbe40eed87fa61fd6926b03b4a751587d6de6
Konstantin Kepke <k.kepke@yadro.com>
2024-10-31 17:19:42 +0300

    Ping-pong and ping-pong with stop_state

--- 2_TASK/ping-pong_ss.cpp
+++ 2_TASK/ping-pong_ss.cpp
@@ -1,13 +1,13 @@
 #include "common_pp.hpp"
 
-static bool running={true};
+#define INEER_STOP_TOKEN
 
 std::counting_semaphore<1> sema(0);
 
-void pleer1 (std::string& name){
+void pleer1 (std::stop_token st, std::string& name){
     Greeting p("_"+name);
     std::this_thread::sleep_for(800ms); // time to pong started and wait ping on acqure()
-    while (running){
+    while (!st.stop_requested()){
         cout << "PING "<< endl;
         sema.release();
         std::this_thread::sleep_for(1500ms);
@@ -15,9 +15,9 @@ void pleer1 (std::string& name){
     sema.release();
 }
 
-void pleer2 (std::string& name){
+void pleer2 (std::stop_token st, std::string& name){
     Greeting p("_"+name);
-    while (running){       
+    while (!st.stop_requested()){       
         if (sema.try_acquire())
         {
             //sema.acquire();
@@ -35,11 +35,20 @@ int main() {
     std::string pl1="Pleer1";
     std::string pl2="Pleer2";
 
-    std::jthread th1(pleer1, std::ref(pl1));
+#ifdef INEER_STOP_TOKEN
+    std::stop_source source;
+    std::jthread th1(pleer1, source.get_token(), std::ref(pl1));
+    std::jthread th2(pleer2, source.get_token(), std::ref(pl2));
+#else
     std::jthread th2(pleer2, std::ref(pl2));
+    std::jthread th1(pleer1, std::ref(pl1));
+#endif
 
-    cout << "Press enter to exit" << endl;
+    
+    cout << "\n\nPress enter to exit !!\n\n" << endl;
     char ch = getchar();
+#ifdef INEER_STOP_TOKEN
+    source.request_stop(); //has to be worked without request_stop() on jthreadd desrtr
+#endif
     cout << "Just quit." << ch << endl;
-    running = false;
 }
